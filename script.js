f8c2827bd84467b71f135b3a7172f39100765a72
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-04 13:57:53 +0300

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Hello!');
+console.log('Hello!'); // function call statement 
+
+
+    var first = 'Enzo';  // Variable declaration statement 
+
+    
+
+    /*
+    Can be updated
+    Scoped to a funcition (function scoped only available in )
+    Since 1995 - previously only way to declare
+
+    - rarely used, better for making the scope bigger
+    */
+
+    
+    let age = '19xx';
+
+    /*
+    Can be updated
+    block scoped
+    introduced in ES6
+
+    - when a const doesnt make sense, so let is used when needed 
+    - usually when a value will need to be updated lated
+    */
+
+    const cool = 'true';
+    /*
+    Const variables cannot and should not be changed
+    ex: API key
+    block scoped
+    introduced in ES6
+
+    - Wes Bos uses const by default when declaring variable
+    */  
