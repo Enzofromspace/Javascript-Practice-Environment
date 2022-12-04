5d8207ff62d49819b5f902f8c00143ef4b8fce35
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-04 14:16:01 +0300

    added notes from vid 7 - types

---
+++ types.js
@@ -0,0 +1,21 @@
+const name = "name";
+const middle = 'middle';
+const last = `last`;
+
+const sentence = "shes so \"cool\"";
+
+/*
+concatination:
+joing strings together
+
+example below
+*/
+
+const hello = 'hello my name is '; + name + "nice to meet you";
+
+/*
+Backtick syntax to easily concatenate strings and variables
+${nameOfVariable} + "string";
+*/
+
+const hello2 = `hello my name is ${name}. Nice to Meet ya!`;
